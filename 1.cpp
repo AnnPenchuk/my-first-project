ebddf684db588eaae1814d43073dbbf247572f4c
AnnPenchuk <annapenchuk95@mail.ru>
2019-04-29 23:24:25 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	int a, b, p;
 	scanf("%d%d", &a, &b);
-	p = a*b;
+	p = a*b*b;
 	printf("pri proizvedenii chisla %d na chislo %d rezultat raven %d\n", a, b, p);
 	system("pause");
 	return 0;
